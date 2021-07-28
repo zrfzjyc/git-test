2479d8ffa1de0b19ba1cb1862e99b58b05e88c11
jin <492650471@qq.com>
2021-07-28 22:33:50 +0800

    first commit

--- src/main/java/com/zt/git/GitTest.java
+++ src/main/java/com/zt/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("hellogit");
+        System.out.println("hellogit1");
     }
 
 }
